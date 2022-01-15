1a78c8af20d2f4eb3a258bb2f038cbea09a8ddc2
kedior <kedior123@gmail.com>
2022-01-15 18:08:12 +0800

    fix Log

--- logEveryday/logger.go
+++ logEveryday/logger.go
@@ -7,7 +7,7 @@ import (
 	"time"
 )
 
-func logEveryday(logger *log.Logger, t time.Time, folderPath string) error {
+func LogEveryday(logger *log.Logger, t time.Time, folderPath string) error {
 	os.MkdirAll(folderPath, os.ModePerm)
 	logFile, err := os.OpenFile(folderPath+t.String()[:10]+".log",
 		os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
@@ -18,7 +18,7 @@ func logEveryday(logger *log.Logger, t time.Time, folderPath string) error {
 	date, month, day := t.Date()
 	next := time.Date(date, month, day+1, 0, 0, 0, 0, time.Local)
 	time.AfterFunc(next.Sub(time.Now())+time.Second, func() {
-		err := logEveryday(logger, next, folderPath)
+		err := LogEveryday(logger, next, folderPath)
 		if err != nil {
 			panic(err)
 		}
