986f9fd8cba9546fcd204958aec51ff1a2efc0e8
kedior <kedior123@gmail.com>
2022-01-18 19:21:13 +0800

    i think it's fun

--- ipLimiter/ip.go
+++ ipLimiter/ip.go
@@ -1,9 +1,56 @@
 package ipLimiter
 
 import (
+	"bytes"
+	"errors"
+	"strconv"
+	"strings"
 	"sync"
 	"time"
 )
+
+var (
+	InvalidIpErr = errors.New("invalid ip string")
+)
+
+type IP uint32
+
+func NewIP(str string) (error, IP) {
+	sp := strings.Split(str, ".")
+	if len(sp) != 4 {
+		return InvalidIpErr, 0
+	}
+	ip := uint32(0)
+	for i := 3; i >= 0; i-- {
+		v, e := strconv.Atoi(sp[i])
+		if e != nil || v > 255 {
+			return InvalidIpErr, 0
+		}
+		ip <<= 8
+		ip |= uint32(v)
+	}
+	return nil, IP(ip)
+}
+
+func MustIP(str string) IP {
+	err, ip := NewIP(str)
+	if err != nil {
+		panic(err)
+	}
+	return ip
+}
+
+func (ip IP) String() string {
+	bf := bytes.NewBufferString("")
+	t := uint32(ip)
+	for i := 0; i < 4; i++ {
+		bf.WriteString(strconv.Itoa(int(t & 255)))
+		bf.WriteRune('.')
+		t >>= 8
+	}
+	bf.Truncate(bf.Len() - 1)
+	return bf.String()
+}
 
 type limiter struct {
 	limitTo int64
@@ -15,10 +62,10 @@ type ipLimiter struct {
 	limitTime time.Duration
 	banTime   time.Duration
 	repeat    uint8
-	m         map[string]*limiter
+	m         map[IP]*limiter
 	mu        sync.Mutex
 	super     *ipLimiter
-	banAction func(ip string)
+	banAction func(ip IP)
 }
 
 // NewIpLimit 创建一个新的limiter。
@@ -27,20 +74,20 @@ type ipLimiter struct {
 //ip被ban时，limiter会调用一次action，并且会向父级limiter的Put方法传入一次ip。
 //super为父级limiter的指针
 func NewIpLimit(limitTime, banTime time.Duration,
-	repeat uint8, action func(string),
+	repeat uint8, action func(IP),
 	super *ipLimiter) *ipLimiter {
 	return &ipLimiter{
 		limitTime: limitTime,
 		repeat:    repeat,
 		banTime:   banTime,
-		m:         make(map[string]*limiter),
+		m:         make(map[IP]*limiter),
 		super:     super,
 		banAction: action,
 	}
 }
 
 // IsBanning 返回ip在时间戳为time的时刻是否被封禁
-func (l *ipLimiter) IsBanning(ip string, time int64) bool {
+func (l *ipLimiter) IsBanning(ip IP, time int64) bool {
 	return (l.m[ip] != nil && l.m[ip].limitTo > time) ||
 		(l.super != nil && l.super.IsBanning(ip, time))
 
@@ -49,7 +96,7 @@ func (l *ipLimiter) IsBanning(ip string, time int64) bool {
 // Put 提醒limiter此ip进行了一次访问
 //如果此一个p多次访问，超过limiter的限制，将会ban掉该ip
 //如果ip已经被ban了，返回false，否则返回true
-func (l *ipLimiter) Put(ip string) bool {
+func (l *ipLimiter) Put(ip IP) bool {
 	l.mu.Lock()
 	defer l.mu.Unlock()
 	now := time.Now().UnixNano()
--- ipLimiter/ip_test.go
+++ ipLimiter/ip_test.go
@@ -10,18 +10,18 @@ import (
 
 var l = NewIpLimit(
 	time.Millisecond*300, time.Second*5, 5,
-	func(ip string) {
+	func(ip IP) {
 		log.Println("ip:", ip, "访问过快,已被临时封禁5秒")
 	}, NewIpLimit(
 		time.Minute*3, time.Hour, 3,
-		func(ip string) {
+		func(ip IP) {
 			log.Println("ip:", ip, "多次访问过快,已被临时封禁1小时")
 		}, nil))
 
 func TestNewIpLimit(t *testing.T) {
-
+	ip := MustIP("127.0.0.1")
 	for i := 0; i < 100; i++ {
-		fmt.Printf("%v  ", l.Put("127.0.0.1"))
+		fmt.Printf("%v  ", l.Put(ip))
 		time.Sleep(time.Millisecond * 290)
 	}
 }
@@ -29,7 +29,8 @@ func TestNewIpLimit(t *testing.T) {
 func TestIpLimiter_Put(t *testing.T) {
 	ipLimit := func(fun http.HandlerFunc) http.HandlerFunc {
 		return func(w http.ResponseWriter, r *http.Request) {
-			if !l.Put(r.RemoteAddr) {
+			_, ip := NewIP("192.168.0.1")
+			if !l.Put(ip) {
 				fmt.Fprint(w, "FAST！")
 				return
 			}
@@ -40,4 +41,9 @@ func TestIpLimiter_Put(t *testing.T) {
 		fmt.Fprint(w, "hello!")
 	}))
 	http.ListenAndServe(":8080", nil)
+}
+
+func TestNewIP(t *testing.T) {
+	_, ip := NewIP("192.98.45.249")
+	fmt.Println(ip)
 }
